f43b886916db5949e9b26424f6d5630548100478
roy-ermers <dangergamesnl@gmail.com>
2021-03-16 22:08:57 +0100

    added base layout

--- pages/homepage.vue
+++ pages/homepage.vue
@@ -3,22 +3,133 @@
 		<!-- HERO -->
 		<section id="hero">
 			<h1 ref="title"></h1>
-			<v-button>Start met vernieuwen</v-button>
+			<v-button href="#contact">Start met vernieuwen</v-button>
 		</section>
 
 		<!-- CALL TO ACTION -->
-		<section>
+		<section class="cta">
 			<grid :gutter="0">
 				<row class="cta">
 					<column :size="12">
 						<h2>{{cta_text}}</h2>
 
-						<v-button type="white">Meer informatie</v-button>
+						<v-button
+							href="#contact"
+							type="white"
+						>Meer informatie</v-button>
 					</column>
 				</row>
 			</grid>
 		</section>
-		<section class="contact">
+		<section class="two-column">
+			<div class="image-round">
+				<img
+					:src="`${$strapi.options.url}uploads/laptop_040706a4f4.png`"
+					alt=""
+				>
+			</div>
+			<div>
+				<markdown
+					:text="getTextblock('first block')"
+					class="text-block"
+				/>
+				<v-button
+					href="#contact"
+					type="orange"
+				>Get started
+					<icon name="arrow" />
+				</v-button>
+			</div>
+		</section>
+		<section class="stats">
+			<h2>Some count that matters</h2>
+			<p>Our achievements in the journey depicted in numbers</p>
+
+			<div class="numbers">
+				<div
+					class="counter"
+					v-for="data in counter"
+					:key="data.id"
+				>
+					<h3>{{data.amount}}</h3>
+					<p>{{data.description}}</p>
+				</div>
+			</div>
+		</section>
+		<section class="two-column">
+			<div class="image-square">
+				<img
+					:src="`${$strapi.options.url}uploads/phone_ff6673d61a.png`"
+					alt=""
+				>
+			</div>
+			<div>
+				<markdown
+					:text="getTextblock('first block')"
+					class="text-block"
+				/>
+				<v-button
+					href="#contact"
+					type="text orange"
+				>Get started
+					<icon name="arrow" />
+				</v-button>
+			</div>
+		</section>
+		<section class="projects">
+			<h2>Selected works</h2>
+			<div class="grid">
+				<article
+					class="project"
+					v-for="project in projects"
+					:key="project.id"
+				>
+					<img
+						:src="`${$strapi.options.url}${project.logo.url.substring(1)}`"
+						:style="{background: project.color}"
+					>
+					<p class="title">{{project.client}} - <strong>{{project.title}}</strong></p>
+					<div class="bottom">
+						<strong>{{project.type}}</strong> <a :href="`/project/${project.slug}`">Read more</a>
+					</div>
+				</article>
+			</div>
+		</section>
+		<section class="cta">
+			<grid :gutter="0">
+				<row class="cta">
+					<column :size="12">
+						<h2>{{cta_text_2}}</h2>
+
+						<v-button
+							type="white"
+							href="#contact"
+						>Meer informatie</v-button>
+					</column>
+				</row>
+			</grid>
+		</section>
+		<section class="two-column">
+			<div>
+				<markdown
+					:text="getTextblock('first block')"
+					class="text-block"
+				/>
+				<v-button
+					href="#contact"
+					type="orange"
+				>Get started
+					<icon name="arrow" />
+				</v-button>
+			</div>
+			<div class="image-square">
+				<img
+					:src="`${$strapi.options.url}uploads/phone_ff6673d61a.png`"
+					alt=""
+				>
+			</div>
+		</section>
+		<section id="contact">
 			<form
 				v-if="!sentContact"
 				:action="`${$strapi.options.url}contacts`"
@@ -55,8 +166,8 @@
 				<textarea
 					name="message"
 					v-model="contact.message"
-          minlength="10"
-          maxlength="512"
+					minlength="10"
+					maxlength="512"
 				></textarea>
 				<p
 					class="error"
@@ -69,6 +180,17 @@
 				<p>We nemen zo snel mogelijk contact op.</p>
 			</div>
 		</section>
+		<footer>
+			<p><img
+					src="~/assets/logo.svg"
+					class="logo"
+				> Divs and devs</p>
+
+			<div>
+				<p>KvK: 000000</p>
+				<p>Btw: 000000</p>
+			</div>
+		</footer>
 	</div>
 </template>
 
@@ -84,11 +206,15 @@
 					subject: '',
 					message: ''
 				},
-				errors: {}
+				errors: {},
+				projects: []
 			}
 		},
 		async asyncData({ $strapi }) {
 			return await $strapi.find('homepage')
+		},
+		async fetch() {
+			this.projects = await this.$strapi.find('projects')
 		},
 		methods: {
 			async saveContact(ev) {
@@ -103,6 +229,11 @@
 					console.error(e.data)
 				}
 			},
+			getTextblock(name) {
+				const block = this.textblock.find(x => x.description == name)
+
+				return block.text
+			},
 			startTypewriter() {
 				const typewriter = new Typewriter(this.$refs.title, {
 					autoStart: true,
@@ -114,16 +245,23 @@
 					.typeString(this.title)
 					.start()
 					.callFunction(() => {
-						document.querySelector('.Typewriter__cursor').remove()
+						const cursor = document.querySelector('.Typewriter__cursor')
+						if (cursor) cursor.remove()
 					})
 			}
 		},
 		mounted() {
-			this.startTypewriter()
+			if (process.browser) this.startTypewriter()
 		}
 	}
 </script>
+<style lang="scss">
 
+html {
+	scroll-behavior: smooth;
+}
+
+</style>
 <style lang="scss" scoped>
 	h1 {
 		max-width: 710px;
@@ -133,11 +271,14 @@
 
 	section {
 		overflow: hidden;
+		padding: 0 10rem;
+		height: 70vh;
 
 		&#hero {
-			max-height: 100vh;
+			height: 100vh;
 			flex-direction: column;
 			position: relative;
+			margin: 0;
 
 			@include flex;
 
@@ -153,13 +294,13 @@
 				bottom: -110%;
 			}
 		}
-	}
 
-	.row {
 		&.cta {
 			background: $accent;
 			border-radius: $border-radius;
-			padding: 2.5rem;
+			padding: 2.5rem 0;
+			height: unset;
+			margin: 0 10rem;
 
 			h2 {
 				margin-top: 0;
@@ -173,6 +314,204 @@
 					color: #fff;
 				}
 			}
+		}
+
+		&.stats {
+			background-color: $accent2;
+			color: #fff;
+			text-align: center;
+			padding: 4rem 0;
+			height: unset;
+
+			h2,
+			p {
+				margin: 0;
+			}
+
+			.numbers {
+				margin-top: 3rem;
+				@include flex(stretch);
+
+				.counter {
+					color: #fff;
+					flex: 1;
+
+					h3 {
+						margin: 0;
+						font-size: 2rem;
+					}
+
+					&:not(:last-of-type) {
+						border-right: 1px solid rgba(255, 255, 255, 0.4);
+					}
+				}
+			}
+		}
+	}
+
+	.projects .grid {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		gap: 7rem;
+		padding: 0 10rem;
+		place-content: center;
+
+		.project {
+			.title {
+				font-size: 1.2rem;
+
+				strong {
+					font-weight: 700;
+				}
+			}
+
+			img {
+				width: 100%;
+				height: 15rem;
+				padding: 1rem;
+				box-sizing: border-box;
+
+				border-radius: 0.5rem;
+
+				object-fit: contain;
+				object-position: center;
+			}
+
+			.bottom {
+				@include flex(space-between);
+				margin: 1.5rem 0;
+
+				strong {
+					font-weight: 700;
+					font-size: 0.875rem;
+					letter-spacing: 2px;
+					text-transform: uppercase;
+				}
+
+				a {
+					display: inline-block;
+					text-decoration: none;
+					background-color: $accent;
+					padding: 0.5rem 1rem;
+					border-radius: 5rem;
+					color: #fff;
+				}
+			}
+		}
+	}
+
+	.two-column {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+
+		padding: 4rem 10rem;
+
+		align-items: center;
+		justify-items: space-between;
+
+		div:last-child {
+			justify-self: end;
+		}
+	}
+
+	.text-block::v-deep {
+		vertical-align: middle;
+
+		em {
+			color: $accent;
+			font-weight: 700;
+			letter-spacing: 2px;
+			text-transform: uppercase;
+			font-style: normal;
+		}
+
+		p {
+			margin: 0;
+		}
+
+		h1 {
+			margin: 0;
+			font-size: 2.5rem;
+			font-weight: 600;
+			margin-bottom: 2rem;
+		}
+	}
+	.image-square {
+		background-color: $accent-light;
+		border-radius: 1rem;
+
+		width: 32rem;
+		height: 32rem;
+
+		@include flex(center, flex-end);
+		padding: 1rem;
+		padding-bottom: 0;
+
+		img {
+			max-width: 100%;
+			max-height: 100%;
+			object-fit: cover;
+			object-position: center;
+		}
+	}
+
+	.image-round {
+		background-color: $accent2-light;
+		border-radius: 100%;
+
+		width: 32rem;
+		height: 32rem;
+
+		@include flex;
+
+		img {
+			max-width: 100%;
+			max-height: 100%;
+			object-fit: contain;
+			object-position: center;
+		}
+	}
+
+	img {
+		-webkit-user-drag: none;
+	}
+
+	footer {
+		position: relative;
+		height: 16rem;
+		background-color: black;
+		color: #fff;
+
+		display: flex;
+
+		place-items: center;
+		place-content: center;
+
+		> p {
+			margin: 5rem;
+		}
+
+		.logo {
+			width: 2.5rem;
+			height: 2.5rem;
+			vertical-align: middle;
+		}
+
+		&::after {
+			content: '';
+			z-index: -1;
+			position: absolute;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+
+			background: url('~assets/logo-white.svg');
+			background-position: left;
+			background-size: 24rem;
+			background-repeat: no-repeat;
+
+			opacity: 0.1;
 		}
 	}
 </style>
